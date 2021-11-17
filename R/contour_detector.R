044f2c5a6f0e8765297e13b75e0eee2788b1cc0b
Jan Wijffels <jwijffels@bnosac.be>
2021-11-17 10:10:02 +0000

    version 0.1.1

--- R/contour_detector.R
+++ R/contour_detector.R
@@ -11,6 +11,7 @@
 #' an efficient algorithm is derived producing sub-pixel contours.
 #' @param x a matrix of image pixel values in the 0-255 range.
 #' @param Q numeric value with the pixel quantization step
+#' @param ... further arguments, not used yet
 #' @return an object of class cld which is a list with the following elements
 #' \itemize{
 #'  \item{curves: }{The number of contour lines found}
@@ -18,21 +19,36 @@
 #'  \item{data: }{A data.frame with columns 'x', 'y' and 'curve' giving the x/y locations for each contour curve}
 #' }
 #' @references Rafael Grompone von Gioi, and Gregory Randall, Unsupervised Smooth Contour Detection, 
-#' Image Processing On Line, 6 (2016), pp. 233-267. \url{https://doi.org/10.5201/ipol.2016.175}
+#' Image Processing On Line, 6 (2016), pp. 233-267. \doi{10.5201/ipol.2016.175}
 #' @export
 #' @examples
+#' \dontshow{
+#' if(require(pixmap))
+#' \{
+#' }
 #' library(pixmap)
 #' imagelocation <- system.file("extdata", "image.pgm", package="image.ContourDetector")
-#' image <- read.pnm(file = imagelocation, cellres = 1)
-#' x <- image@grey * 255
-#' contourlines <- image_contour_detector(x)
+#' image         <- read.pnm(file = imagelocation, cellres = 1)
+#' \dontshow{
+#' image <- image[1:100, 1:100] ## speedup to have CRAN R CMD check within 5 secs
+#' }
+#' x             <- image@grey * 255
+#' contourlines  <- image_contour_detector(x, Q = 2)
 #' contourlines
 #' plot(image)
 #' plot(contourlines, add = TRUE, col = "red")
+#' \dontshow{
+#' \}
+#' # End of main if statement running only if the required packages are installed
+#' }
 #' 
 #' ##
 #' ## line_segment_detector expects a matrix as input
 #' ##  if you have a jpg/png/... convert it to pgm first or take the r/g/b channel
+#' \dontshow{
+#' if(require(magick))
+#' \{
+#' }
 #' library(magick)
 #' x   <- image_read(system.file("extdata", "atomium.jpg", package="image.ContourDetector"))
 #' x
@@ -41,7 +57,36 @@
 #' mat <- drop(mat)
 #' contourlines <- image_contour_detector(mat)
 #' plot(contourlines)
-image_contour_detector <- function(x, Q=2.0) {
+#' \dontshow{
+#' \}
+#' # End of main if statement running only if the required packages are installed
+#' }
+#' 
+#' ##
+#' ##  working with a RasterLayer
+#' ##
+#' \dontshow{
+#' if(require(raster))
+#' \{
+#' }
+#' \donttest{
+#' library(raster)
+#' x   <- raster(system.file("extdata", "landscape.tif", package="image.ContourDetector"))
+#' 
+#' contourlines <- image_contour_detector(x)
+#' image(x)
+#' plot(contourlines, add = TRUE, col = "blue", lwd = 10)
+#' }
+#' \dontshow{
+#' \}
+#' # End of main if statement running only if the required packages are installed
+#' }
+image_contour_detector <- function(x, Q=2.0, ...){
+  UseMethod("image_contour_detector")
+}
+
+#' @export
+image_contour_detector.matrix <- function(x, Q=2.0, ...){
   stopifnot(is.matrix(x))
   contourlines <- detect_contours(x, 
                   X=nrow(x),
@@ -55,7 +100,26 @@ image_contour_detector <- function(x, Q=2.0) {
   contourlines$data <- data.frame(x = contourlines$y, y = nrow(x) - contourlines$x, curve = curve)
   contourlines <- contourlines[c("curves", "contourpoints", "data")]
   class(contourlines) <- "cld"
-  contourlines
+  return(contourlines)
+}
+
+#' @export
+image_contour_detector.RasterLayer <- function(x, Q=2.0, ...){
+  requireNamespace("raster")
+  minX = raster::extent(x)[1]
+  minY = raster::extent(x)[3]  
+  resol = raster::res(x)[1]  
+  x = raster::as.matrix(x)
+  
+  if( anyNA(x) ){
+    x[is.na(x)] = 0
+    warning("NA values found and set to 0") }
+
+  contourlines = image_contour_detector.matrix(x, Q=Q)
+  
+  contourlines$data$x = contourlines$data$x * resol + minX
+  contourlines$data$y = contourlines$data$y * resol + minY
+  return(contourlines)
 }
 
 
@@ -74,12 +138,18 @@ print.cld <- function(x, ...){
 #' @return invisibly a SpatialLines object with the contour lines
 #' @export 
 #' @examples 
+#' \dontshow{
+#' if(require(pixmap))\{
+#' }
 #' library(pixmap)
 #' imagelocation <- system.file("extdata", "image.pgm", package="image.ContourDetector")
-#' image <- read.pnm(file = imagelocation, cellres = 1)
-#' contourlines <- image_contour_detector(image@grey * 255)
+#' image         <- read.pnm(file = imagelocation, cellres = 1)
+#' contourlines  <- image_contour_detector(image@grey * 255)
 #' plot(image)
 #' plot(contourlines, add = TRUE, col = "red")
+#' \dontshow{
+#' \} # End of main if statement running only if the required packages are installed
+#' }
 plot.cld <- function(x, ...){
   requireNamespace("sp")
   l <- split(x$data, x$data$curve)
